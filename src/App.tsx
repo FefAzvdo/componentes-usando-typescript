434757b08f3e944a7773678997df236eed4ee4e5
Fernando Augusto <wfernandoaugusto@gmail.com>
2022-09-16 21:18:02 -0300

    Criado componente Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import Button from "./components/Button";
 import ProgressBar from "./components/ProgressBar";
-
+import { colors } from "../utils/colors";
 //aqui
 export default function App() {
-  return <ProgressBar />;
+  return (
+    <div>
+      {/* <ProgressBar /> */}
+      <Button
+        size="medium"
+        onClick={() => alert("clicou !!")}
+        icon={<div>OK</div>}
+        bgColor={colors.DeepPink}
+      >
+        Click me
+      </Button>
+    </div>
+  );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,52 @@
-export default function Button() {
-  return <button>{"Click me"}</button>;
+import { ButtonHTMLAttributes } from "react";
+import styled from "styled-components";
+
+type StyledButtonProps = {
+  bgColor?: string;
+  size?: "small" | "medium" | "big";
+};
+
+const StyledButton = styled.button<StyledButtonProps>`
+  border: none;
+  padding: ${(props) => {
+    if (props.size === "small") {
+      return "15px 20px";
+    } else if (props.size === "medium") {
+      return "15px 40px";
+    } else if (props.size === "big") {
+      return "15px 80px";
+    }
+  }};
+  background-color: ${(props) => props.bgColor};
+  text-align: center;
+  font-family: "Courier New", Courier, monospace;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: ${(props) => props.bgColor + "bf"};
+  }
+`;
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  bgColor?: string;
+  size?: "small" | "medium" | "big";
+  icon: JSX.Element;
+  onClick: () => any;
+}
+
+export default function Button({
+  bgColor,
+  size,
+  icon,
+  children,
+  onClick,
+}: ButtonProps) {
+  return (
+    <StyledButton bgColor={bgColor} size={size} onClick={onClick}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <span style={{ marginRight: "10px" }}>{children}</span>
+        {icon}
+      </div>
+    </StyledButton>
+  );
 }
