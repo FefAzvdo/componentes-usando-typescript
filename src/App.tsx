f7161845f4c0ba9bf976ed94997cad329a9ee539
Fernando Augusto <wfernandoaugusto@gmail.com>
2022-09-17 13:38:56 -0300

    Adicionado Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import Button from "./components/Button";
-import ProgressBar from "./components/ProgressBar";
 import { colors } from "../utils/colors";
-//aqui
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TestingPage from "./pages/TestingPage";
+
 export default function App() {
   return (
-    <div>
-      {/* <ProgressBar /> */}
-      <Button
-        size="medium"
-        onClick={() => alert("clicou !!")}
-        icon={<div>OK</div>}
-        bgColor={colors.DeepPink}
-      >
-        Click me
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TestingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
